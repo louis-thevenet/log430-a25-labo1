0e67f1b4b201126ad95080351c14a29c021c7d49
Louis Thevenet <louis.tvnt@gmail.com>
2025-09-17 08:22:11 -0400

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Python development shell";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              docker
+              docker-compose
+              (python3.withPackages (python-pkgs: [ python-pkgs.pytest ]))
+            ];
+          };
+        };
+    };
+}
